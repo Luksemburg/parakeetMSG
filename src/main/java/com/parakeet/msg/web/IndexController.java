bcce4b9137c5c92a62bbbe138d2c808bff58b76e
Luksemburg <42497183+Luksemburg@users.noreply.github.com>
2019-07-23 03:59:16 +0300

    Add files via upload

---
+++ src/main/java/com/parakeet/msg/web/IndexController.java
@@ -0,0 +1,22 @@
+package com.parakeet.msg.web;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController implements ErrorController {
+
+	private static final String PATH = "WEB-INF/classes/templates/error.html";
+	
+	@RequestMapping(value = PATH)
+    public String error() {
+        return "Error handling";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
