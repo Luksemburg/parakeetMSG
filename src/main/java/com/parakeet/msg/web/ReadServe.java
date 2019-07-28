59b09835bcd8aa39eeb7890dbc779c4bfa81db0a
Luksemburg <42497183+Luksemburg@users.noreply.github.com>
2019-07-28 03:25:58 +0300

    Add files via upload

---
+++ src/main/java/com/parakeet/msg/web/ReadServe.java
@@ -0,0 +1,28 @@
+package com.parakeet.msg.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/read")
+public class ReadServe extends HttpServlet {
+
+	private static final long serialVersionUID = -8449943618556927643L;
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		AdapterDB helper = (AdapterDB) session.getAttribute("helper");
+		String login = (String)session.getAttribute("login");
+    	String client = (String)session.getAttribute("client");
+		
+		helper.toZero(login, client);
+	}
+
+}
